f6b1baa1b95a03c4bbd97b9af3b3207adcd3ee85
Alex940404 <78331203+Alex940404@users.noreply.github.com>
2021-02-01 16:58:09 +0800

    Add files via upload

---
+++ D1-99.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:24:15 2021
+
+@author: MTAEXAM
+"""
+
+for n in range(1,10,2):
+    print(n)
+for n in range(1,10):
+    for m in range(1,10):
+        i=n*m
+        print(i)
+for A in range(1,,):
+    print(A)
---
+++ D1-n.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:53:00 2021
+
+@author: MTAEXAM
+"""
+total=1
+n=int(input("n:"))
+for n in range(1,n+1):
+    total=total*n
+print(total)
