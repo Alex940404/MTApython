97a4a336fbd0fc7627f6ef967b9ed54dc780c179
Alex940404 <78331203+Alex940404@users.noreply.github.com>
2021-02-01 10:45:30 +0800

    Add files via upload

---
+++ D1-area.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:18 2021
+
+@author: MTAEXAM
+"""
+
+hight = float(input("高:"))
+bottom = float(input("下底:"))
+top = float(input("上底"))
+area = (top+bottom)*hight/2
+print(area)
