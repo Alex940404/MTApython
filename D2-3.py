a3d2ebf6d9a1272445e157bbb1641dd85fa87c9f
Alex940404 <78331203+Alex940404@users.noreply.github.com>
2021-02-02 15:25:01 +0800

    Add files via upload

---
+++ D2-3.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 09:46:28 2021
+
+@author: MTAEXAM
+"""
+
+x = 0
+while x < 4:
+    if x - 2 < 0:
+        print("cake")
+    elif x / 3 == 0:
+        print("greeting")
+    else:
+        print("birthday")
+        x = x + 1
